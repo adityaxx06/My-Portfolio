94b1d0c3bf8877552c08b4207014d47cba49d7ba
Aditya Soni <adisonii2004@gmail.com>
2025-07-31 17:37:32 +0530

    form work change

--- script.js
+++ script.js
@@ -32,13 +32,13 @@ const msg = document.getElementById("msg");
 
 form.addEventListener('submit', e => {
 e.preventDefault()
-fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+msg.innerHTML = "Message Sent Successfully..!";
+fetch(scriptURL, { method: 'POST', body: new FormData(form)},form.reset())
     .then(response => {
-     msg.innerHTML = "Message Sent Successfully..!";
     setTimeout(function(){
         msg.innerHTML = "";
     },5000)
-    form.reset();
+    
     })
     .catch(error => console.error('Error!', error.message))
 })
