d7eb6112017a73279879b60fa6c3595cd31609da
Aditya Soni <adisonii2004@gmail.com>
2025-08-17 17:41:51 +0530

    modification

--- script.js
+++ script.js
@@ -63,3 +63,7 @@ window.onclick = function(event) {
   });
 };
 
+// Scroll Reveal animations
+ScrollReveal().reveal('.section h2', { delay: 200, origin: 'top', distance: '30px', duration: 800 });
+ScrollReveal().reveal('.card', { interval: 200, origin: 'bottom', distance: '50px', duration: 800 });
+ScrollReveal().reveal('.hero-content', { delay: 300, origin: 'top', distance: '40px', duration: 1000 });
